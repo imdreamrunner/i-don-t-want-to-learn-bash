05eea2d5be02236310094fdf144aab538ac463ae
Zhou Xinzi <imdreamrunner@gmail.com>
2015-02-02 23:27:30 +0800

    add arugument description

--- google.sh
+++ google.sh
@@ -1,4 +1,5 @@
 # Search Google and print links
+# arguments: <key+words>
 wget -U firefox -O - "https://www.google.com/search?q=$1" 2>/dev/null | \
   grep -o '<a href="/url?q=[^"'"'"']*&amp;sa' | \
   sed -e 's/^<a href=["'"'"']\/url?q=//' -e 's/&amp;sa$//'
--- google2.sh
+++ google2.sh
@@ -1,7 +1,8 @@
 # Search Google and save result
+# arguments: <key+words>
 URL=https://www.google.com/search?q=$1
 echo Downloading from $URL
-result=$(wget -U firefox -O - $URL 2>/dev/null |   grep -o '<a href="/url?q=[^"'"'"']*&amp;sa' |   sed -e 's/^<a href=["'"'"']\/url?q=//' -e 's/&amp;sa$//')
+result=$(wget -U firefox -O - $URL 2>/dev/null | grep -o '<a href="/url?q=[^"'"'"']*&amp;sa' | sed -e 's/^<a href=["'"'"']\/url?q=//' -e 's/&amp;sa$//')
 
 i=1
 for r in $result
